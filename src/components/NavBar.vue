5ffff35e4b0113f0f9938add4193ee0eb3868eca
Rajesh Budhathoki <rajoyish@gmail.com>
2023-12-16 20:43:46 +0545

    Init with Router and Tailwind CSS

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="flex flex-col text-lg">
+    <RouterLink
+      to="/"
+      class="hover:bg-indigo-100 hover:text-indigo-600 p-4 transition-colors duration-300"
+    >
+      Home
+    </RouterLink>
+
+    <RouterLink
+      to="/about"
+      class="hover:bg-indigo-100 hover:text-indigo-600 p-4 transition-colors duration-300"
+    >
+      About
+    </RouterLink>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="p-8 max-w-xl mx-auto space-y-8">
+    <h1 class="text-3xl font-bold text-center">Master Tailwind CSS UI Components</h1>
+    <NavBar />
+  </main>
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
